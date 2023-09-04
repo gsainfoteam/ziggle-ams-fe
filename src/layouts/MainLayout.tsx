d6e683688be890aee85f014e47f29c6cc792fd88
2paperstar <lee@paperst.ar>
2023-09-05 08:55:10 +0900

    feat: fill rest of page area with main content (Outlet)

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,13 +2,23 @@ import { Outlet } from "react-router";
 import Header from "src/components/Header";
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
+const Main = styled.main`
+  flex: 1;
+`;
 
 const MainLayout = () => {
   return (
     <Container>
       <Header />
-      <Outlet />
+      <Main>
+        <Outlet />
+      </Main>
     </Container>
   );
 };
