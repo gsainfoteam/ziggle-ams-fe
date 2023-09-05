360fdf01742f0168dc7a642c5c724fd44b620923
2paperstar <lee@paperst.ar>
2023-09-05 10:23:59 +0900

    feat: add board page

---
+++ src/pages/Board/index.tsx
@@ -0,0 +1,5 @@
+const Board = () => {
+  return <>board page</>;
+};
+
+export default Board;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainLayout from "src/layouts/MainLayout";
+import Board from "src/pages/Board";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainLayout />} />
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Board />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
