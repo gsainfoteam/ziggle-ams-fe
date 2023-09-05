9e995e70533e7683acdeb2a025bd307761f3bfdc
2paperstar <lee@paperst.ar>
2023-09-05 10:26:36 +0900

    feat: add dashboard page

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainLayout from "src/layouts/MainLayout";
 import BoardPage from "src/pages/Board";
 import CreateProjectPage from "src/pages/CreateProject";
+import DashboardPage from "src/pages/Dashboard";
 
 const Router = () => {
   return (
@@ -10,6 +11,9 @@ const Router = () => {
         <Route path="/" element={<MainLayout />}>
           <Route index element={<BoardPage />} />
           <Route path="create" element={<CreateProjectPage />} />
+          <Route path=":id">
+            <Route index element={<DashboardPage />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
