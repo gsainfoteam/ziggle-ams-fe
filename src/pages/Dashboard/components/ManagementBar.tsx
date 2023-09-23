949bcd9b32239f0492a61931eecf68a8bed16819
qkralthf0115 <qkralthf0115@naver.com>
2023-09-23 23:00:08 +0900

    🎨 responsive design

--- src/pages/Dashboard/components/ManagementBar.tsx
+++ src/pages/Dashboard/components/ManagementBar.tsx
@@ -6,13 +6,9 @@ import plus from "../assets/plus.png";
 import profile from "../assets/profile.png";
 import { Logo } from "./Common";
 
-const Management = styled.div`
-  display: flex;
-  justify-content: space-around;
-`;
-
 const StatusContainer = styled.div`
   display: flex;
+  flex: 1;
   flex-direction: column;
   background-color: white;
   border-radius: 3px;
@@ -22,11 +18,26 @@ const StatusContainer = styled.div`
   padding: 30px 80px;
 `;
 
-const Status = styled.section`
+const Flex = styled.div`
   display: flex;
+`;
+
+const Management = styled.section`
+  display: flex;
+  flex: 1;
   flex-direction: column;
   align-items: center;
-  padding: 0 80px;
+  padding: 0px 10px;
+`;
+
+const Status = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 15px;
+  @media screen and (max-width: 850px) {
+    flex-direction: column;
+  }
 `;
 
 const Border = styled.div`
@@ -38,51 +49,58 @@ const Change = styled.section`
   width: 50px;
   height: 27;
   background-color: #ffdddd;
-  border-radius: 20px;
+  border-radius: 13px;
+  width: 58px;
+  height: 27px;
 `;
 
 const ManagementBar = () => {
   return (
     <StatusContainer>
-      <Management>
-        <Border>
+      <Flex>
+        <Management>
+          <Logo>
+            <img src={caution} alt="caution icon" />
+            <h3>마감기한</h3>
+          </Logo>
           <Status>
-            <Logo>
-              <img src={caution} alt="caution icon" />
-              <h3>마감기한</h3>
-            </Logo>
             <h2>props</h2>
           </Status>
-        </Border>
-        <Border>
+        </Management>
+        <Border />
+        <Management>
+          <Logo>
+            <img src={profile} alt="profile icon" />
+            <h3>지원자 수</h3>
+          </Logo>
           <Status>
-            <Logo>
-              <img src={profile} alt="profile icon" />
-              <h3>지원자 수</h3>
-            </Logo>
-            <h2>props</h2>
-            <Change />
-          </Status>
-        </Border>
-        <Border>
-          <Status>
-            <Logo>
-              <img src={doughnut} alt="doughnut icon" />
-              <h3>경쟁률</h3>
-            </Logo>
             <h2>props</h2>
             <Change />
           </Status>
-        </Border>
-        <Status>
+        </Management>
+        <Border />
+        <Management>
+          <Logo>
+            <img src={doughnut} alt="doughnut icon" />
+            <h3>경쟁률</h3>
+          </Logo>
+          <Status>
+            <h2>props</h2>
+            <Change />
+          </Status>
+        </Management>
+        <Border />
+        <Management>
           <Logo>
             <img src={plus} alt="plus icon" />
             <h3>조회수</h3>
           </Logo>
-          <h2>props</h2>
-          <Change />
-        </Status>
-      </Management>
+          <Status>
+            <h2>props</h2>
+            <Change />
+          </Status>
+        </Management>
+      </Flex>
     </StatusContainer>
   );
 };
