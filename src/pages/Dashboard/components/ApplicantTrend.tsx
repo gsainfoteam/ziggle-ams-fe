99b1f2a793fbed4611d936073896da2f9641edd5
qkralthf0115 <qkralthf0115@naver.com>
2023-09-24 16:13:28 +0900

    💄 chart

--- src/pages/Dashboard/components/ApplicantTrend.tsx
+++ src/pages/Dashboard/components/ApplicantTrend.tsx
@@ -1,6 +1,7 @@
 import "../initChartJS";
 
 import dayjs from "dayjs";
+import { useEffect, useState } from "react";
 // import { SubTitle } from "chart.js";
 import { Bar, Chart } from "react-chartjs-2";
 import styled from "styled-components";
@@ -21,16 +22,27 @@ const ChartContainer = styled.div`
   position: relative;
 
   canvas {
-    height: 300px;
+    height: 200px;
   }
 `;
 const ApplicantTrend = () => {
-  const label = [...Array(7)].map((element, index) => {
-    return dayjs()
-      .subtract(index + 1, "days")
-      .format("MMM DD");
-  });
-  const data = [9, 9, 2, 14, 7, 9, 2];
+  const label = [...Array(7)]
+    .map((element, index) => {
+      return dayjs()
+        .subtract(index + 1, "days")
+        .format("MMM DD");
+    })
+    .reverse();
+  const [data, setData] = useState([9, 9, 2, 14, 7, 9, 2]);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setData([...Array(7)].map(() => Math.floor(Math.random() * 15)));
+    }, 1e3 * 60);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
 
   return (
     <Trend>
@@ -45,8 +57,32 @@ const ApplicantTrend = () => {
             options={{
               responsive: true,
               scales: {
-                x: { grid: { display: false } },
-                y: { position: "right" },
+                x: {
+                  grid: {
+                    display: false,
+                  },
+                },
+                y: {
+                  position: "right",
+                  ticks: { stepSize: 5 },
+                  grid: {
+                    lineWidth: (context) => (context.index === 0 ? 2 : 1),
+                    color: (context) =>
+                      context.index === 0 ? "Gray" : "LightGray",
+                  },
+                  border: { display: false },
+                },
+              },
+
+              plugins: {
+                tooltip: {
+                  yAlign: "bottom",
+                  callbacks: {
+                    title: () => [],
+                    label: (context) => `${context.parsed.y}명`,
+                  },
+                  displayColors: false,
+                },
               },
               maintainAspectRatio: false,
             }}
@@ -66,6 +102,8 @@ const ApplicantTrend = () => {
                   backgroundColor: "#FCD2D2",
                   borderColor: "#FCD2D2",
                   hoverBackgroundColor: "#eb6263",
+                  borderRadius: { topLeft: 8, topRight: 8 },
+                  barThickness: 30,
                   data: data,
                 },
               ],
