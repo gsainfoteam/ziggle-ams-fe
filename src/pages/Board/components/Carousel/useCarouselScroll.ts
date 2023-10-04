32e7bb4b08a169c2ab82559714fef85386d57d4c
ParkJumyung <parkjumyungpublic@gmail.com>
2023-10-04 20:35:28 +0900

    💄 BoardPage: Carousel scroll

---
+++ src/pages/Board/components/Carousel/useCarouselScroll.ts
@@ -0,0 +1,44 @@
+import { useRef } from "react";
+
+function useCarouselScroll({
+  next,
+  prev,
+  threshold = 5,
+}: {
+  next: () => void;
+  prev: () => void;
+  threshold?: number;
+}) {
+  const scrollTimestamp = useRef(Date.now());
+
+  const onWheel = (e: React.WheelEvent<HTMLDivElement>) => {
+    const now = Date.now();
+    const timeSinceLastScrollinMs = now - scrollTimestamp.current;
+    if (timeSinceLastScrollinMs > 500) {
+      scrollTimestamp.current = now;
+      if (e.deltaX > threshold) {
+        next();
+      }
+      if (e.deltaX < -threshold) {
+        prev();
+      }
+    }
+  };
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    console.log(e.key);
+    if (e.key === "ArrowRight") {
+      next();
+    }
+    if (e.key === "ArrowLeft") {
+      prev();
+    }
+  };
+
+  return {
+    onWheel,
+    onKeyDown,
+  };
+}
+
+export default useCarouselScroll;
