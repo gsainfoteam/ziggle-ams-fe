b37926518c7d619b0c6f0ee302ec13167b865518
ParkJumyung <parkjumyungpublic@gmail.com>
2024-01-05 23:55:04 +0900

    🎨 converted to styled-components

--- src/pages/Board/components/Carousel/AddProjectCard.tsx
+++ src/pages/Board/components/Carousel/AddProjectCard.tsx
@@ -1,19 +1,24 @@
 import { MdOutlineAdd } from "react-icons/md";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 import CircularButton from "../CircularButton";
 import Paper from "./Paper";
 
+const LinkContainer = styled(Link)`
+  text-decoration: none;
+`;
+
 function AddProjectCard({ focused }: { focused: boolean }) {
   return (
-    <Link to="/create" style={{ textDecoration: "none" }}>
+    <LinkContainer to="/create">
       <Paper focused={focused} outline>
         <h1>새 모집 프로젝트 시작하기</h1>
         <CircularButton color={"lightgray"}>
           <MdOutlineAdd size={50} />
         </CircularButton>
       </Paper>
-    </Link>
+    </LinkContainer>
   );
 }
 
